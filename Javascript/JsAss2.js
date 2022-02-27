87fa550d9689c70ef2ae7496b73b757a98dd1831
shyn14 <97467317+shyn14@users.noreply.github.com>
2022-02-27 11:37:49 +0530

    Update JsAss2.js

--- Javascript/JsAss2.js
+++ Javascript/JsAss2.js
@@ -1,3 +1,4 @@
+/*Create a function that takes an array of strings and returns an array with only the strings that have numbers in them. If there are no strings containing numbers, return an empty array.*/
 function check(arr){
     // let str='sad';
     let arr2=[];
